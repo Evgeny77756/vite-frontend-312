3ad1876920c4f331777aecb25a52134437fe186f
Evgeny <evgeny1989starikov@mail.ru>
2023-10-25 19:04:46 +0300

    Исправит предупреждения линтера для css, js. Добавит новые правила

--- src/js/tasks.js
+++ src/js/tasks.js
@@ -71,21 +71,17 @@ function initTasksSwiper() {
 // Skeleton
 window.addEventListener("load", () => {
     const skeletons = document.querySelectorAll(".skeleton");
+
     skeletons.forEach(function (element) {
         element.classList.remove("skeleton");
     });
 });
 
 // Иконки heart /
-const hearts = document.querySelectorAll('.card-heart');
 const hearts = document.querySelectorAll(".card-heart");
 
-hearts.forEach(heart => {
-    heart.addEventListener('click', () => {
-        heart.classList.toggle('heart-active');
 hearts.forEach((heart) => {
     heart.addEventListener("click", () => {
         heart.classList.toggle("heart-active");
     });
-});
 });
