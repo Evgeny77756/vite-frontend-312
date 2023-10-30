b1005c03b1b752cc2c95912a6ed1827279f2854f
Evgeny <evgeny1989starikov@mail.ru>
2023-10-30 23:23:02 +0300

    Переопределит стили swiper. Добавит лого

--- src/js/tasks.js
+++ src/js/tasks.js
@@ -6,41 +6,11 @@ const GLOBAL = {
   currentPage: window.location.pathname,
 }
 
-/**
- * Функция отрисовки данных
- * @param {string} filterValue - значение фильтра по категории
- * @param {string} searchValue - значение поиска
- */
-const renderData = (filterValue, searchValue) => {
+// Функция отрисовки карточек
+const renderData = () => {
   const swiperWrapper = document.querySelector('.swiper-wrapper')
 
-  swiperWrapper.innerHTML = '' // Очистка существующих карточек
-
-  /**
-   * Функция фильтрации и поиска данных
-   * @param {string} filterValue - значение фильтра по категории
-   * @param {string} searchValue - значение поиска
-   * @returns {Array} Отфильтрованный массив данных
-   */
-  const filterAndSearch = (filterValue, searchValue) => {
-    return data.filter((product) => {
-      // проверка фильтрации по категории
-      if (filterValue !== 'all' && product.category !== filterValue) {
-        // исключаем товары, к-ые не соотв. выбранной категории
-        return false
-      }
-      // проверка поиска по названию товара (без учета регистра)
-      // if(searchValue) {
-
-      // }
-      // если товар прошел оба условия, то он остается в результате фильтрации
-      return true
-    })
-  }
-
-  const filteredData = filterAndSearch(filterValue, searchValue)
-
-  filteredData.forEach(function (product) {
+  data.forEach(function (product) {
     const div = document.createElement('div') // Создать дивку
     div.classList.add('swiper-slide') // Добавить класс дивке
 
@@ -115,12 +85,8 @@ const renderData = (filterValue, searchValue) => {
 
     swiperWrapper.appendChild(div) // Вставка сформированной дивки в swiperWrapper
   })
-
   initTasksSwiper() // Вызов функции для инициализации слайдера
 }
-
-// вызов функции для первичной отрисовки
-renderData('all', '')
 
 function displayTasksDetails() {
   const taskUrlId = window.location.search.split('=')[1] // извлекаем id из адресной строки
